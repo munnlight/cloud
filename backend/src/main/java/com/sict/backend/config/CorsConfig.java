138365a7950df1c0cedcead265406337bddb34cc
monhtulga <122615139+munnlight@users.noreply.github.com>
2025-04-16 03:57:53 +0800

    Update CorsConfig.java

--- backend/src/main/java/com/sict/backend/config/CorsConfig.java
+++ backend/src/main/java/com/sict/backend/config/CorsConfig.java
@@ -9,7 +9,10 @@ public class CorsConfig implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
-        .allowedOrigins("http://localhost:3000")
+        .allowedOrigins(
+          "http://localhost:3000",
+          "https://cloudsict.vercel.app"
+        )
         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
         .allowedHeaders("*")
         .allowCredentials(true);
