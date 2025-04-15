e5f9f50abac15558d801dbeb0fa1ca3fc5db34f3
monhtulga <munkhtulga.es@gmail.com>
2025-04-16 03:45:38 +0800

    Demo project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [data, setData] = useState<any[]>([]);
+
+  useEffect(() => {
+    const handleGet = async () => {
+      const response = await fetch("http://localhost:8080/users");
+      const data = await response.json();
+      console.log(data);
+      setData(data);
+    };
+
+    handleGet();
+  }, []);
+
+  return (
+    <div
+      style={{
+        margin: "auto",
+        textAlign: "center",
+        fontWeight: 800,
+        alignItems: "center",
+        alignContent: "center",
+        height: "100vh",
+      }}
+    >
+      <div>It worked here is the data:</div>
+      <div>
+        {data.map((item: any, index: number) => (
+          <div key={index}>
+            <p>
+              {item.uid} {item.name} {item.age}
+            </p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
